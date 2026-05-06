aecf6a29e5166c1e6a756d4e6b589c798c1f93fb
Chukwuebuka (Gaus Octavio ) <56232734+Chukwuebuka-2003@users.noreply.github.com>
2026-05-06 13:36:13 +0100

    Update entrypoint.prod.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DJANGO_SETTINGS_MODULE=test_project.settings_prod
 set -e
 
 echo "Running migrations..."
@@ -10,8 +11,9 @@ python manage.py collectstatic --noinput
 echo "Creating superuser if needed..."
 python manage.py shell <<EOF
 from django.contrib.auth.models import User
+import os
 if not User.objects.filter(username='admin').exists():
-    u = User.objects.create_superuser('admin', '', '$ADMIN_PASSWORD')
+    User.objects.create_superuser('admin', '', os.environ.get('ADMIN_PASSWORD', 'admin123'))
     print("Superuser created.")
 else:
     print("Superuser already exists.")
